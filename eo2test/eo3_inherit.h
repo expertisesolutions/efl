cbfaa938554f72bbe4d581d3b0444669473f784f
Felipe Magno de Almeida <eu@felipemagno.com.br>
2013-10-16 14:28:48 -0300

    eo3: Implemented EO3_CONSTRUCTOR, EO3_CONSTRUCTOR_OVERRIDE and EO3_DESTRUCTOR

--- eo2test/eo3_inherit.h
+++ eo2test/eo3_inherit.h
@@ -4,8 +4,8 @@
 #include "Eo.h"
 
 #define EO3_INHERIT_CLASS eo3_inherit_class                             \
-    , EO3_FUNCTION_OVERRIDE(eo2_constructor, _constructor, void, int)   \
-    , EO3_FUNCTION_OVERRIDE(eo2_destructor, _destructor, void)          \
+    , EO3_CONSTRUCTOR_OVERRIDE(eo2_constructor, _constructor)           \
+    , EO3_DESTRUCTOR(_destructor)                                       \
     , EO3_FUNCTION_OVERRIDE(simple_inc, _inc, void, int)                \
     , EO3_FUNCTION(eo3_inherit_get, _get, int)                          \
     , EO3_FUNCTION_OVERRIDE(simple_virtual, _virtual, int, int)         \
--- eo2test/eo3_simple.h
+++ eo2test/eo3_simple.h
@@ -4,8 +4,8 @@
 #include "Eo.h"
 
 #define EO3_SIMPLE_CLASS eo3_simple                                  \
-    , EO3_FUNCTION(simple_constructor, _constructor, void, int)      \
-    , EO3_FUNCTION_OVERRIDE(eo2_destructor, _destructor, void)       \
+    , EO3_CONSTRUCTOR(simple_constructor, _constructor, int)         \
+    , EO3_DESTRUCTOR(_destructor)                                    \
     , EO3_FUNCTION(simple_inc, _inc, void)                           \
     , EO3_FUNCTION(simple_get, _get, int)                            \
     , EO3_FUNCTION(simple_set, _set, void, int)                      \
