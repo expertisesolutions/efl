9e159567137df3258d4d3301cf28cfca1dbd7122
Larry <larry.olj@gmail.com>
2013-10-31 00:28:59 -0200

    fix in constructor class signature

--- eo2test/eo3_no_instant.h
+++ eo2test/eo3_no_instant.h
@@ -3,9 +3,9 @@
 
 #include "Eo.h"
 
-#define EO3_NO_INSTANT eo3_no_instant                            		 \
-    , function(get_inc_id, _get_inc_id, unsigned int)                   	 \
-    , class_constructor(no_instant_class_constructor, _class_contructor, void)   \
+#define EO3_NO_INSTANT eo3_no_instant                         \
+    , function(get_inc_id, _get_inc_id, unsigned int)         \
+    , class_constructor(_class_contructor)                    \
 
 
 EO3_DECLARE_CLASS(EO3_NO_INSTANT)
