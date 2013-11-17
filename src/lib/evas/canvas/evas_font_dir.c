560868a3404ad9b4c6dedf08bc16526806db1b6c
Cedric BAIL <cedric@efl.so>
2013-11-17 04:47:58 +0100

    evas: fix build without FontConfig.
    Note that we still support building without FontConfig, so please
    guard access to it.

--- src/lib/evas/canvas/evas_font_dir.c
+++ src/lib/evas/canvas/evas_font_dir.c
@@ -1220,7 +1220,7 @@ _canvas_font_path_clear(Eo *eo_e EINA_UNUSED, void *_pd, va_list *list EINA_UNUS
 	evas->font_path = eina_list_remove(evas->font_path, evas->font_path->data);
      }
 
-#ifdef HAVE_FONTCONFIG        
+#ifdef HAVE_FONTCONFIG
    if (fc_config)
       FcConfigAppFontClear(fc_config);
 #endif
@@ -1244,11 +1244,10 @@ _canvas_font_path_append(Eo *eo_e EINA_UNUSED, void *_pd, va_list *list)
    e->font_path = eina_list_append(e->font_path, eina_stringshare_add(path));
 
    evas_font_init();
-
-#ifdef HAVE_FONTCONFIG        
+#ifdef HAVE_FONTCONFIG
    if (fc_config)
       FcConfigAppFontAddDir(fc_config, (const FcChar8 *) path);
-#endif   
+#endif
 }
 
 EAPI void
@@ -1269,8 +1268,7 @@ _canvas_font_path_prepend(Eo *eo_e EINA_UNUSED, void *_pd, va_list *list)
    e->font_path = eina_list_prepend(e->font_path, eina_stringshare_add(path));
 
    evas_font_init();
-
-#ifdef HAVE_FONTCONFIG        
+#ifdef HAVE_FONTCONFIG
    if (fc_config)
       FcConfigAppFontAddDir(fc_config, (const FcChar8 *) path);
 #endif
