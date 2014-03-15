22d2b6fec704d8dfc01ca77c7cb3c4d734c0ddf2
Carlos Carvalho <ccarvalho@expertisesolutions.com.br>
2014-03-15 14:40:08 -0300

    emodel: Base interface.

--- src/lib/emodel/Emodel.h
+++ src/lib/emodel/Emodel.h
@@ -28,28 +28,28 @@ extern "C" {
  *
  * @{
  */
-#define EMODEL_OBJ_CLASS emodel_obj_class_get()
-const Eo_Class *emodel_obj_class_get(void) EINA_CONST;
-extern EAPI Eo_Op EMODEL_OBJ_BASE_ID;
+#define EMODEL_CLASS emodel_class_get()
+const Eo_Class *emodel_class_get(void) EINA_CONST;
+extern EAPI Eo_Op EMODEL__BASE_ID;
 
 enum
 {
-   EMODEL_OBJ_SUB_ID_CONSTRUCTOR,
-   EMODEL_OBJ_SUB_ID_PROPERTIES_GET,
-   EMODEL_OBJ_SUB_ID_PROPERTY_GET,
-   EMODEL_OBJ_SUB_ID_PROPERTY_SET,
-   EMODEL_OBJ_SUB_ID_LOAD,
-   EMODEL_OBJ_SUB_ID_UNLOAD,
-   EMODEL_OBJ_SUB_ID_CHILD_ADD,
-   EMODEL_OBJ_SUB_ID_CHILDREN_GET,
-   EMODEL_OBJ_SUB_ID_CHILDREN_SLICE_GET,
-   EMODEL_OBJ_SUB_ID_CHILDREN_COUNT_GET,
-   EMODEL_OBJ_SUB_ID_LAST,
+   EMODEL_SUB_ID_CONSTRUCTOR,
+   EMODEL_SUB_ID_PROPERTIES_GET,
+   EMODEL_SUB_ID_PROPERTY_GET,
+   EMODEL_SUB_ID_PROPERTY_SET,
+   EMODEL_SUB_ID_LOAD,
+   EMODEL_SUB_ID_UNLOAD,
+   EMODEL_SUB_ID_CHILD_ADD,
+   EMODEL_SUB_ID_CHILDREN_GET,
+   EMODEL_SUB_ID_CHILDREN_SLICE_GET,
+   EMODEL_SUB_ID_CHILDREN_COUNT_GET,
+   EMODEL_SUB_ID_LAST,
 };
 
-typedef void (*Emodel_Child_Add_Cb)(Eo *child, void *data);
+typedef void (*Emodel_Child_Add_Cb)(void *data, Eo *child, void *event_info);
 
-#define EMODEL_OBJ_ID(sub_id) (EMODEL_OBJ_BASE_ID + sub_id)
+#define EMODEL_ID(sub_id) (EMODEL_BASE_ID + sub_id)
 
 /**
  * @def emodel_constructor
@@ -58,70 +58,70 @@ typedef void (*Emodel_Child_Add_Cb)(Eo *child, void *data);
  * Contructor for Emodel.
  *
  */
-#define emodel_constructor() EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CONSTRUCTOR)
+#define emodel_constructor() EMODEL_ID(EMODEL_SUB_ID_CONSTRUCTOR)
 
 /**
  * @def emodel_properties_get
  * @since 1.9
  *
  */
-#define emodel_properties_get() EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_PROPERTIES_GET)
+#define emodel_properties_get() EMODEL_ID(EMODEL_SUB_ID_PROPERTIES_GET)
 
 /**
  * @def emodel_property_get
  * @since 1.9
  *
  */
-#define emodel_property_get(property) EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_PROPERTY_GET), EO_TYPECHECK(const char*, property)
+#define emodel_property_get(property) EMODEL_ID(EMODEL_SUB_ID_PROPERTY_GET), EO_TYPECHECK(const char*, property)
 
 /**
  * @def emodel_property_set
  * @since 1.9
  *
  */
-#define emodel_property_set(property) EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_PROPERTY_SET), EO_TYPECHECK(const char*, property)
+#define emodel_property_set(property) EMODEL_ID(EMODEL_SUB_ID_PROPERTY_SET), EO_TYPECHECK(const char*, property), EO_TYPECHECK(Eina_Value *, value)
 
 /**
  * @def emodel_load
  * @since 1.9
  *
  */
-#define emodel_load() EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_LOAD)
+#define emodel_load() EMODEL_ID(EMODEL_SUB_ID_LOAD)
 
 /**
  * @def emodel_unload
  * @since 1.9
  *
  */
-#define emodel_unload() EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_UNLOAD)
+#define emodel_unload() EMODEL_ID(EMODEL_SUB_ID_UNLOAD)
 
 /**
  * @def emodel_child_add
  * @since 1.9
  *
  */
-#define emodel_child_add(child_add_cb, data) EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILD_ADD), EO_TYPECHECK(Emodel_Child_Add_Cb*, child_add_cd), EO_TYPECHECK(void*, data)
+#define emodel_child_add(child_add_cb, data) EMODEL_ID(EMODEL_SUB_ID_CHILD_ADD), EO_TYPECHECK(Emodel_Child_Add_Cb*, child_add_cd), EO_TYPECHECK(void*, data)
 
 /**
  * @def emodel_children_get
  * @since 1.9
  *
  */
-#define emodel_children_get() EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILDREN_GET)
+#define emodel_children_get() EMODEL__ID(EMODEL_SUB_ID_CHILDREN_GET)
 
 /**
  * @def emodel_children_slice_get
  * @since 1.9
  *
  */
-#define emodel_children_slice_get(start, count) EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILDREN_SLICE_GET), EO_TYPECHECK(int, start), EO_TYPECHECK(int, cont)
+#define emodel_children_slice_get(start, count) EMODEL_ID(EMODEL_SUB_ID_CHILDREN_SLICE_GET), EO_TYPECHECK(int, start), EO_TYPECHECK(int, cont)
 
 /**
  * @def emodel_children_count_get
  * @since 1.9
  *
  */
-#define emodel_children_count_get() EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILDREN_COUNT_GET)
+#define emodel_children_count_get() EMODEL_ID(EMODEL_SUB_ID_CHILDREN_COUNT_GET)
 
 extern const Eo_Event_Description _EMODEL_PROPERTIES_CHANGE_EVT;
 #define EMODEL_PROPERTIES_CHANGE_EVT (&(_EMODEL_PROPERTIES_CHANGE_EVT))
--- src/lib/emodel/emodel.c
+++ src/lib/emodel/emodel.c
@@ -1,19 +1,16 @@
 #include <Emodel.h>
+#include <Eina.h>
 
-EAPI Eo_Op EMODEL_OBJ_BASE_ID = EO_NOOP;
+EAPI Eo_Op EMODEL_BASE_ID = EO_NOOP;
 
-#define MY_CLASS EMODEL_OBJ_CLASS
+#define MY_CLASS EMODEL_CLASS
 #define MY_CLASS_NAME "Emodel"
 
-struct _E_Model
-{
-   // TODO: Implement this
-};
 
 typedef struct _E_Model E_Model;
 
 EAPI const Eo_Event_Description _EMODEL_PROPERTIES_CHANGE_EVT =
-   EO_EVENT_DESCRIPTION("emodel, properties change","List properties.");
+   EO_EVENT_DESCRIPTION("emodel, properties change","Properties list is available.");
 
 EAPI const Eo_Event_Description _EMODEL_PROPERTY_CHANGE_EVT =
    EO_EVENT_DESCRIPTION("emodel, property change","Property value is available.");
@@ -24,96 +21,17 @@ EAPI const Eo_Event_Description _EMODEL_CHILD_ADD_EVT =
 EAPI const Eo_Event_Description _EMODEL_CHILD_DEL_EVT =
    EO_EVENT_DESCRIPTION("emodel, child del","Child was removed.");
 
-static void
-_emodel_constructor(Eo *obj, void *class_data, va_list *list)
-{
-   eo_do_super(obj, MY_CLASS, eo_constructor());
-}
-
-static void
-_emodel_properties_get(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_property_get(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_property_set(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_load(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_unload(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_child_add(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_children_get(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_children_slice_get(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_emodel_children_count_get(Eo *obj, void *class_data, va_list *list)
-{
-
-}
-
-static void
-_class_constructor(Eo_Class *klass)
-{
-   const Eo_Op_Func_Description func_desc[] = {
-      EO_OP_FUNC(EO_BASE_ID(EO_BASE_SUB_ID_CONSTRUCTOR), _emodel_constructor),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_PROPERTIES_GET), _emodel_properties_get),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_PROPERTY_GET), _emodel_property_get),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_PROPERTY_SET), _emodel_property_set),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_LOAD), _emodel_load),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_UNLOAD), _emodel_unload),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILD_ADD), _emodel_child_add),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILDREN_GET), _emodel_children_get),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILDREN_SLICE_GET), _emodel_children_slice_get),
-      EO_OP_FUNC(EMODEL_OBJ_ID(EMODEL_OBJ_SUB_ID_CHILDREN_COUNT_GET), _emodel_children_count_get),
-      EO_OP_FUNC_SENTINEL
-   };
-
-   eo_class_funcs_set(klass, func_desc);
-}
 
 static const Eo_Op_Description op_desc[] = {
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_PROPERTIES_GET, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_PROPERTY_GET, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_PROPERTY_SET, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_LOAD, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_UNLOAD, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_CHILD_ADD, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_CHILDREN_GET, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_CHILDREN_SLICE_GET, ""),
-      EO_OP_DESCRIPTION(EMODEL_OBJ_SUB_ID_CHILDREN_COUNT_GET, ""),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_PROPERTIES_GET, "Get list of properties."),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_PROPERTY_GET, "Get value from given property."),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_PROPERTY_SET, "Set property value."),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_LOAD, "Load emodel"),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_UNLOAD, "Unload emodel"),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_CHILD_ADD, "Add new child."),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_CHILDREN_GET, "Get all children."),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_CHILDREN_SLICE_GET, "Get children from given slice/range."),
+      EO_OP_DESCRIPTION(EMODEL_SUB_ID_CHILDREN_COUNT_GET, "Count number of children."),
       EO_OP_DESCRIPTION_SENTINEL
 };
 
@@ -128,12 +46,12 @@ static const Eo_Event_Description *event_descs[] = {
 static const Eo_Class_Description class_desc = {
      EO_VERSION,
      MY_CLASS_NAME,
-     EO_CLASS_TYPE_REGULAR,
-     EO_CLASS_DESCRIPTION_OPS(&EMODEL_OBJ_BASE_ID, op_desc, EMODEL_OBJ_SUB_ID_LAST),
+     EO_CLASS_TYPE_INTERFACE,
+     EO_CLASS_DESCRIPTION_OPS(&EMODEL_BASE_ID, op_desc, EMODEL_SUB_ID_LAST),
      event_descs,
-     sizeof(E_Model),
-     _class_constructor,
+     0,
+     NULL,
      NULL
 };
 
-EO_DEFINE_CLASS(emodel_obj_class_get, &class_desc, EO_BASE_CLASS, NULL);
+EO_DEFINE_CLASS(emodel_class_get, &class_desc, NULL, NULL);
