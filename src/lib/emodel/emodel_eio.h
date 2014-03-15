d4d5b92eecabfd35f40aec3ec3c86f64f8095456
Larry <larry.olj@gmail.com>
2014-03-15 15:48:11 -0300

    added contructor class

--- src/lib/emodel/emodel_eio.h
+++ src/lib/emodel/emodel_eio.h
@@ -29,6 +29,22 @@ const Eo_Class *emodel_obj_eio_class_get(void) EINA_CONST;
 
 extern EAPI Eo_Op EMODEL_OBJ_EIO_BASE_ID;
 
+enum
+{
+   EMODEL_OBJ_EIO_SUB_ID_CONSTRUCTOR,
+   EMODEL_OBJ_EIO_SUB_ID_LAST,
+};
+
+/**
+ * @def emodel_constructor
+ * @since 1.9
+ *
+ * Contructor for Emodel.
+ * @param[in] path to file
+ *
+ */
+#define emodel_eio_constructor(path) EMODEL_ID(EMODEL_SUB_ID_CONSTRUCTOR), EO_TYPECHECK(const char *, path)
+
 #ifdef __cplusplus
 }
 #endif
