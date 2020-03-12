4f538a81ec798f0bb198b486cd29500d00c8e953
Lucas Cavalcante de Sousa <lucas@expertisesolutions.com.br>
2020-03-12 14:20:11 -0300

    remove alguns esboços relacionados ao uso de dispositivos de sincronização

--- src/lib/evil/unposix/pthread.h
+++ src/lib/evil/unposix/pthread.h
@@ -5,13 +5,7 @@
 
 UNIMPLEMENTED typedef unsigned long long pthread_t;
 
-UNIMPLEMENTED_STRUCT_T(pthread_mutexattr)
-UNIMPLEMENTED_STRUCT_T(pthread_condattr)
 UNIMPLEMENTED_STRUCT_T(pthread_attr)
-UNIMPLEMENTED_STRUCT_T(pthread_rwlock)
-UNIMPLEMENTED_STRUCT_T(pthread_cond)
-UNIMPLEMENTED_STRUCT_T(pthread_mutex)
-UNIMPLEMENTED_STRUCT_T(sem)
 
 UNIMPLEMENTED struct sched_param {
     int sched_priority;
@@ -32,12 +26,6 @@ UNIMPLEMENTED typedef int pthread_key_t;
 #define PTHREAD_CREATE_JOINABLE 8
 #define PTHREAD_EXPLICIT_SCHED 9
 #define PTHREAD_INHERIT_SCHED 10
-#define PTHREAD_MUTEX_DEFAULT 11
-#define PTHREAD_MUTEX_ERRORCHECK 12
-#define PTHREAD_MUTEX_NORMAL 13
-#define PTHREAD_MUTEX_RECURSIVE 14
-#define PTHREAD_MUTEX_ROBUST 15
-#define PTHREAD_MUTEX_STALLED 16
 #define PTHREAD_ONCE_INIT 17
 #define PTHREAD_PRIO_INHERIT 18
 #define PTHREAD_PRIO_NONE 19
@@ -46,96 +34,6 @@ UNIMPLEMENTED typedef int pthread_key_t;
 #define PTHREAD_PROCESS_PRIVATE 22
 #define PTHREAD_SCOPE_PROCESS 23
 #define PTHREAD_SCOPE_SYSTEM 24
-
-UNIMPLEMENTED inline int pthread_mutex_trylock(void* m)
-{
-    #warning pthread_mutex_trylock is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_mutex_lock(void* m)
-{
-    #warning pthread_mutex_lock is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_mutex_unlock(void* m)
-{
-    #warning pthread_mutex_unlock is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_cond_wait(void* c, void* m)
-{
-    #warning pthread_cond_wait is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_cond_timedwait(void* c, void* m, int t)
-{
-    #warning pthread_cond_timedwait is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_cond_broadcast(void* c)
-{
-    #warning pthread_cond_broadcast is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_cond_signal(void* c)
-{
-    #warning pthread_cond_signal is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_rwlock_rdlock(void* c)
-{
-    #warning pthread_rwlock_rdlock is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_rwlock_wrlock(void* c)
-{
-    #warning pthread_rwlock_wrlock is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_rwlock_unlock(void* c)
-{
-    #warning pthread_rwlock_unlock is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_key_create(void* c, void * d)
-{
-    #warning pthread_key_create is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_key_delete(void* c)
-{
-    #warning pthread_key_delete is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_getspecific(void* c)
-{
-    #warning pthread_getspecific is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_setspecific(void* c, const void* d)
-{
-    #warning pthread_setspecific is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_mutexattr_init(void* c, ...)
-{
-    #warning pthread_mutexattr_init is not implemented.
-    return 0;
-}
 
 UNIMPLEMENTED inline int pthread_getschedparam(void* a, ...)
 {
@@ -152,60 +50,6 @@ UNIMPLEMENTED inline int pthread_self()
 UNIMPLEMENTED inline int pthread_setschedparam(void* c, void* d, void* e)
 {
     #warning pthread_ is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_rwlock_init(void* a, ...)
-{
-    #warning pthread_rwlock_init is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_mutexattr_settype(void* a, ...)
-{
-    #warning pthread_mutexattr_settype is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_mutex_init(void* a, ...)
-{
-    #warning pthread_mutex_init is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_mutex_destroy(void* a, ...)
-{
-    #warning pthread_mutex_destroy is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_condattr_init(void* a, ...)
-{
-    #warning pthread_condattr_init is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_cond_init(void* a, ...)
-{
-    #warning pthread_cond_init is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_condattr_destroy(void* a, ...)
-{
-    #warning pthread_condattr_destroy is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_cond_destroy(void* a, ...)
-{
-    #warning pthread_cond_destroy is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_rwlock_destroy(void* a, ...)
-{
-    #warning pthread_rwlock_destroy is not implemented.
     return 0;
 }
 
@@ -266,12 +110,6 @@ UNIMPLEMENTED inline int pthread_equal(void* a, ...)
 UNIMPLEMENTED inline int pthread_setcanceltype(void* a, ...)
 {
     #warning pthread_setcanceltype is not implemented.
-    return 0;
-}
-
-UNIMPLEMENTED inline int pthread_mutexattr_destroy(void* a, ...)
-{
-    #warning pthread_mutexattr_destroy is not implemented.
     return 0;
 }
 
