c3ae61d16077de6fd1d4630c23b7cb00b9cc30f8
caiosignor <caiosignor@gmail.com>
2020-03-18 11:54:31 -0300

    resolving dep unistd

--- src/lib/evil/unposix/unistd.h
+++ src/lib/evil/unposix/unistd.h
@@ -1,16 +1,21 @@
 #ifndef UNISTD_H
 #define UNISTD_H
 
-#include "unimplemented.h"
+//#include "unimplemented.h"
 
 #include <evil_unistd.h>
 
 #define F_OK 0
+#define X_OK 0
 #define W_OK 2
 #define R_OK 4
-#define X_OK 0
 
-UNIMPLEMENTED extern char *optarg;
-UNIMPLEMENTED extern int optind, opterr, optopt;
+char *optarg = 0;
+int optind  = 1, opterr  = 0, optopt  = 0;
+
+
+
+//UNIMPLEMENTED extern char *optarg;
+//UNIMPLEMENTED extern int optind, opterr, optopt;
 
 #endif
