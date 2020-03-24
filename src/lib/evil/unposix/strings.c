a5a6cb554a61c2b0fe482d4e5d0f02bae14b47c1
carlos <caiosignor@gmail.com>
2020-03-24 11:36:58 -0300

    Add implementation of ffs(int i)
    add free to avoid memory leak

---
+++ src/lib/evil/unposix/strings.c
@@ -0,0 +1,35 @@
+#include "strings.h"
+
+inline int ffs(int i)
+{
+	for (int j = 0; j < sizeof(i)*8; j++)
+	{
+		if (i & (1 << j))
+			return j;
+	}
+	return 0;
+}
+
+inline int strcasecmp(const char *s1, const char *s2)
+{
+	return lstrcmpiA(s1, s1);
+}
+
+inline int strcasecmp_l(const char *s1, const char *s2, locale_t locale)
+{
+	//CompareStringW()
+}
+inline int strncasecmp(const char *s1, const char *s2, size_t size)
+{
+	LPTSTR *n_bytes_s1 = (LPTSTR)malloc(size + 1);
+	LPTSTR *n_bytes_s2 = (LPTSTR)malloc(size + 1);
+
+	lstrcpyA(n_bytes_s1, s1);
+	lstrcpyA(n_bytes_s2, s2);
+
+	free(n_bytes_s1);
+	free(n_bytes_s2);
+
+	return lstrcmpiA(n_bytes_s1, n_bytes_s2);
+}
+inline int strncasecmp_l(const char *s1, const char *s2, size_t size, locale_t locale);
