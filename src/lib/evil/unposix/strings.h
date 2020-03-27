85e501e1bf42e1dfa6c4ab878154d2949a808600
carlos <caiosignor@gmail.com>
2020-03-27 19:05:51 -0300

    implemented functions strcasecmp_l; strncasecmp_l and add strings.c to meson.build

--- src/lib/evil/unposix/strings.h
+++ src/lib/evil/unposix/strings.h
@@ -2,29 +2,17 @@
 #define UNPOSIX_STRINGS_H
 
 #include <sys/types.h>
-#include <locale.h>
+#include "locale.h"
 
-inline int ffs(int i)
-{
-    #warning ffs is not implemented.
-    return 0;
-}
 
+#ifndef WIN32_LEAN_AND_MEAN
+# define WIN32_LEAN_AND_MEAN 
+#endif
+#include <Windows.h>
+
+inline int ffs(int i);
 inline int strcasecmp(const char* s1, const char* s2);
 inline int strncasecmp(const char* s1, const char* s2, size_t size);
-
-inline int strcasecmp_l(const char* s1, const char* s2, locale_t locale)
-{
-    #warning strcasecmp_l is not implemented.
-    return 0;
-}
-
-
-
-inline int strncasecmp_l(const char* s1, const char* s2, size_t size, locale_t locale)
-{
-    #warning strncasecmp_l is not implemented.
-    return 0;
-}
-
+inline int strcasecmp_l(const char* s1, const char* s2, locale_t locale);
+inline int strncasecmp_l(const char* s1, const char* s2, size_t size, locale_t locale);
 #endif
