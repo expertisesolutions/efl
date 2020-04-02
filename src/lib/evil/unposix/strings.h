23f507623848c2192bd5cc7a0b1ba033c8da5354
carlos <caiosignor@gmail.com>
2020-04-02 16:36:12 -0300

    Implementation according joão antonio sugestions

--- src/lib/evil/unposix/strings.h
+++ src/lib/evil/unposix/strings.h
@@ -1,18 +1,15 @@
 #ifndef UNPOSIX_STRINGS_H
 #define UNPOSIX_STRINGS_H
 
-#include <sys/types.h>
-#include "locale.h"
-
-
 #ifndef WIN32_LEAN_AND_MEAN
 # define WIN32_LEAN_AND_MEAN 
 #endif
 #include <Windows.h>
+#include_next<string.h>
 
-inline int ffs(int i);
-inline int strcasecmp(const char* s1, const char* s2);
-inline int strncasecmp(const char* s1, const char* s2, size_t size);
-inline int strcasecmp_l(const char* s1, const char* s2, locale_t locale);
-inline int strncasecmp_l(const char* s1, const char* s2, size_t size, locale_t locale);
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#define strncasecmp_l _strnicmp_l
+#define strcasecmp_l _strncmp_l
+inline int ffs(int i)
 #endif
