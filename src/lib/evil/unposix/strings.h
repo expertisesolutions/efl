50aadb7b65135e79d4650626c800865336d46e3f
carlos <caiosignor@gmail.com>
2020-04-02 17:24:11 -0300

    Removed Windows.h

--- src/lib/evil/unposix/strings.h
+++ src/lib/evil/unposix/strings.h
@@ -1,15 +1,11 @@
 #ifndef UNPOSIX_STRINGS_H
 #define UNPOSIX_STRINGS_H
 
-#ifndef WIN32_LEAN_AND_MEAN
-# define WIN32_LEAN_AND_MEAN 
-#endif
-#include <Windows.h>
 #include_next<string.h>
 
 #define strncasecmp _strnicmp
 #define strcasecmp _stricmp
 #define strncasecmp_l _strnicmp_l
 #define strcasecmp_l _strncmp_l
-inline int ffs(int i)
+inline int ffs(int i);
 #endif
