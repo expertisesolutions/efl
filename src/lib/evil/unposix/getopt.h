6c1f73c9cee99652d650f8d482d99d21d29a748b
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2020-04-03 13:53:52 -0300

    Windows: Add getopt implementation
    Summary:
    ========
    Add getopt from https://github.com/nanoporetech/getopt-win32 but with some
    modifications for better suite our purpose, like removing wchar
    implementation;
    
    - `src/lib/evil/unposix/getopt.c` implements getopt, getopt_long and
      getopt_long_only - EFL only use the first and second, but I'm unsure if
      should remove the latter;
    
    - `src/tests/evil/unposix/evil_unposix_test_getopt.c` implements a little teste
      case for getopt.c - it is not a unit tests case;
    
    - For now getopt.h should be included as `#include "getopt.h"` - i.e
      with quotes, not brakets;
    
    Test Plan:
    ==========
    For testing the getopt:
    - As everything that evil_unposix_test_getopt.c, getopt.{c,h} are unplemented at
      unposix (for now at least) they should include Windows CRT versions and not
      unposix ones.
    - With evil_unposix_test_getopt.c and getopt.{c,h} at current dir:
    ```
       clang-cl evil_unposix_test_getopt.c getopt.c -DSTATIC_GETOPT
    ```
      Where `-DSTATIC_GETOPT` export getopt.{c,h} variables and functions for static
      building;
    - Now you can test getopt by passing args to the exe, e.g:
    ```
      evil_unposix_getopt.exe -0 -1 -2 --create banana
    ```
      Where the possible short and long options are defined at
      evil_unposix_getopt.c
    
    For testing if everything else is ok:
    - Comment `Wno-implicit-function-declaration` at meson.build;
    - Run `configure.bat`;
    - Run `build -k0`, it should only return error/warnigs that
      `devs/expertise/native-windows` does

--- src/lib/evil/unposix/getopt.h
+++ src/lib/evil/unposix/getopt.h
@@ -1,28 +1,111 @@
-#ifndef UNPOSIX_GETOPT_H
-#define UNPOSIX_GETOPT_H
+/* Getopt for Microsoft C
+This code is a modification of the Free Software Foundation, Inc.
+Getopt library for parsing command line argument the purpose was
+to provide a Microsoft Visual C friendly derivative. This code
+provides functionality for both Unicode and Multibyte builds.
+Date: 02/03/2011 - Ludvik Jerabek - Initial Release
+Version: 1.0
+Comment: Supports getopt, getopt_long, and getopt_long_only
+and POSIXLY_CORRECT environment flag
+License: LGPL
+Revisions:
+02/03/2011 - Ludvik Jerabek - Initial Release
+02/20/2011 - Ludvik Jerabek - Fixed compiler warnings at Level 4
+07/05/2011 - Ludvik Jerabek - Added no_argument, required_argument, optional_argument defs
+08/03/2011 - Ludvik Jerabek - Fixed non-argument runtime bug which caused runtime exception
+08/09/2011 - Ludvik Jerabek - Added code to export functions for DLL and LIB
+02/15/2012 - Ludvik Jerabek - Fixed _GETOPT_THROW definition missing in implementation file
+08/01/2012 - Ludvik Jerabek - Created separate functions for char and wchar_t characters so single dll can do both unicode and ansi
+10/15/2012 - Ludvik Jerabek - Modified to match latest GNU features
+06/19/2015 - Ludvik Jerabek - Fixed maximum option limitation caused by option_a (255) and option_w (65535) structure val variable
+**DISCLAIMER**
+THIS MATERIAL IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING, BUT Not LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT. SOME JURISDICTIONS DO NOT ALLOW THE
+EXCLUSION OF IMPLIED WARRANTIES, SO THE ABOVE EXCLUSION MAY NOT
+APPLY TO YOU. IN NO EVENT WILL I BE LIABLE TO ANY PARTY FOR ANY
+DIRECT, INDIRECT, SPECIAL OR OTHER CONSEQUENTIAL DAMAGES FOR ANY
+USE OF THIS MATERIAL INCLUDING, WITHOUT LIMITATION, ANY LOST
+PROFITS, BUSINESS INTERRUPTION, LOSS OF PROGRAMS OR OTHER DATA ON
+YOUR INFORMATION HANDLING SYSTEM OR OTHERWISE, EVEN If WE ARE
+EXPRESSLY ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+*/
+#ifndef __GETOPT_H_
+	#define __GETOPT_H_
 
-#include "unimplemented.h"
+	#ifdef _GETOPT_API
+		#undef _GETOPT_API
+	#endif
 
-#include <unistd.h>
+	#if defined(EXPORTS_GETOPT) && defined(STATIC_GETOPT)
+		#error "The preprocessor definitions of EXPORTS_GETOPT and STATIC_GETOPT can only be used individually"
+	#elif defined(STATIC_GETOPT)
+		#define _GETOPT_API
+	#elif defined(EXPORTS_GETOPT)
+		#pragma message("Exporting getopt library")
+		#define _GETOPT_API __declspec(dllexport)
+	#else
+		#pragma message("Importing getopt library")
+		#define _GETOPT_API __declspec(dllimport)
+	#endif
 
-UNIMPLEMENTED int getopt(int argc, char * const argv[],
-          const char *optstring)
-{
-#warning getopt is not implemented
-}
+	// Change behavior for C\C++
+	#ifdef __cplusplus
+		#define _BEGIN_EXTERN_C extern "C" {
+		#define _END_EXTERN_C }
+		#define _GETOPT_THROW throw()
+	#else
+		#define _BEGIN_EXTERN_C
+		#define _END_EXTERN_C
+		#define _GETOPT_THROW
+	#endif
 
-#include_next <getopt.h>
+	// Standard GNU options
+	#define	null_argument		0	/*Argument Null*/
+	#define	no_argument			0	/*Argument Switch Only*/
+	#define required_argument	1	/*Argument Required*/
+	#define optional_argument	2	/*Argument Optional*/
 
-UNIMPLEMENTED int getopt_long(int argc, char * const argv[],
-          const char *optstring,
-          const struct option *longopts, int *longindex)
-{
-#warning getopt_long is not implemented
-}
+	// Shorter Options
+	#define ARG_NULL	0	/*Argument Null*/
+	#define ARG_NONE	0	/*Argument Switch Only*/
+	#define ARG_REQ		1	/*Argument Required*/
+	#define ARG_OPT		2	/*Argument Optional*/
 
-UNIMPLEMENTED int getopt_long_only(int argc, char * const argv[],
-          const char *optstring,
-          const struct option *longopts, int *longindex)
-{
-#warning getopt_long_only is not implemented
-}
+	#include <../ucrt/string.h>
+
+_BEGIN_EXTERN_C
+
+	extern _GETOPT_API int optind;
+	extern _GETOPT_API int opterr;
+	extern _GETOPT_API int optopt;
+
+	// Ansi
+	struct option
+	{
+		const char* name;
+		int has_arg;
+		int *flag;
+		int val;
+	};
+	extern _GETOPT_API char *optarg;
+	extern _GETOPT_API int getopt(int argc, char *const *argv
+                               , const char *optstring) _GETOPT_THROW;
+	extern _GETOPT_API int getopt_long(int argc, char *const *argv
+                                    , const char *options
+                                    , const struct option *long_options
+                                    , int *opt_index) _GETOPT_THROW;
+	extern _GETOPT_API int getopt_long_only(int argc, char *const *argv
+                                         , const char *options
+                                         , const struct option *long_options
+                                         , int *opt_index) _GETOPT_THROW;
+
+_END_EXTERN_C
+
+	#undef _BEGIN_EXTERN_C
+	#undef _END_EXTERN_C
+	#undef _GETOPT_THROW
+	#undef _GETOPT_API
+
+#endif  // __GETOPT_H_
