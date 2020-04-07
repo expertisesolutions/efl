ca119b80a29c48c882cbbaf364347c01fd3dfd3c
carlos <caiosignor@gmail.com>
2020-04-07 14:32:51 -0300

    Some unistd functions are defined into {io,process}.h

--- src/lib/evil/unposix/unistd.h
+++ src/lib/evil/unposix/unistd.h
@@ -1,23 +1,17 @@
 #ifndef UNISTD_H
 #define UNISTD_H
 
-//#include "unimplemented.h"
+#include "unimplemented.h"
 
 #include <evil_unistd.h>
-
+#include <io.h>
+#include <process.h>
 #define F_OK 0
 #define X_OK 0
 #define W_OK 2
 #define R_OK 4
-
-int getopt(int argc, char * const argv[], const char *optstring);
-
-char *optarg = 0;
-int optind  = 1, opterr  = 0, optopt  = 0;
-
-
-
-//UNIMPLEMENTED extern char *optarg;
-//UNIMPLEMENTED extern int optind, opterr, optopt;
+#define STDIN_FILENO  0 
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
 
 #endif
