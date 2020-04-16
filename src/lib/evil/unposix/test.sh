d3cfc9c5f51faaf766364f750c70cdb63ed7e365
João Antônio Cardoso <joao.maker@gmail.com>
2020-04-16 20:03:01 -0300

    changing to test on windows

--- src/lib/evil/unposix/test.sh
+++ src/lib/evil/unposix/test.sh
@@ -1,2 +1,5 @@
-#\rm -f fnmatch_test && gcc -D ORIGINAL_FNMATCH fnmatch_test.c -o fnmatch_test && ./fnmatch_test
-\rm -f fnmatch_test && gcc fnmatch_test.c fnmatch.c -lpcreposix -lpcre -o fnmatch_test && ./fnmatch_test
+#\rm -f fnmatch_test && gcc fnmatch_test.c fnmatch.c -lpcreposix -lpcre -o fnmatch_test && ./fnmatch_test
+PCRE_SRC_DIR="/c/Users/joaoantoniocardo/Desktop/pcre-win-build"
+PCRE_BIN_DIR=$PCRE_SRC_DIR/build-VS2019/x64/Release
+PATH=$PATH:$PCRE_BIN_DIR
+\rm -f fnmatch_test.exe && clang-cl -w -o fnmatch_test.exe fnmatch_test.c fnmatch.c $PCRE_BIN_DIR/libpcre.lib $PCRE_BIN_DIR/libpcreposix.lib -I $PCRE_BIN_DIR -I $PCRE_SRC_DIR && chmod +x fnmatch_ test.exe && ./fnmatch_test.exe
