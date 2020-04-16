bfba5d8d2d86ebd8fee9cfdeffe3b3e0435acfb4
João Antônio Cardoso <joao.maker@gmail.com>
2020-04-16 19:29:35 -0300

    moving to the right place

--- src/lib/evil/unposix/fnmatch.h
+++ src/lib/evil/unposix/fnmatch.h
@@ -1,27 +1,21 @@
-#ifndef UNPOSIX_FNMATCH_H
-#define UNPOSIX_FNMATCH_H
+#ifndef FNMATCH_H
+#define FNMATCH_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <Shlwapi.h>
+#include <pcreposix.h>
 
-// The string does not match the specified pattern.
-#define FNM_NOMATCH 1
-// <slash> in string only matches <slash> in pattern.
-#define FNM_PATHNAME (1 << 0)
-// Disable backslash escaping.
-#define FNM_NOESCAPE (1 << 1)
-// Leading <period> in string must be exactly matched by <period> in pattern.
-#define FNM_PERIOD (1 << 2)
+/* Bits set in the FLAGS argument to `fnmatch'.  */
+#define	FNM_PATHNAME	(1 << 0) /* No wildcard can ever match `/'.  */
+#define	FNM_NOESCAPE	(1 << 1) /* Backslashes don't quote special chars.  */
+#define	FNM_PERIOD	    (1 << 2) /* Leading `.' is matched only explicitly.  */
 
-//#define FNM_IGNORECASE	# optional
-//#define FNM_CASEFOLD      	# optional
+#define	FNM_FILE_NAME	FNM_PATHNAME /* Preferred GNU name.  */
+#define	FNM_LEADING_DIR	(1 << 3) /* Ignore `/...' after a match.  */
+#define	FNM_CASEFOLD	(1 << 4) /* Compare without regard to case.  */
 
-int fnmatch(const char *, const char *, int);
+/* Value returned by `fnmatch' if STRING does not match PATTERN.  */
+#define	FNM_NOMATCH	1
 
-#ifdef __cplusplus
-}
-#endif
+int fnmatch (const char *pattern, const char *string, int flags);
+>>>>>>> b294ea595c... moving to the right place
 
 #endif
--- test.sh
+++
@@ -1,2 +0,0 @@
-#\rm -f fnmatch_test && gcc -D ORIGINAL_FNMATCH fnmatch_test.c -o fnmatch_test && ./fnmatch_test
-\rm -f fnmatch_test && gcc fnmatch_test.c fnmatch.c -lpcreposix -lpcre -o fnmatch_test && ./fnmatch_test
