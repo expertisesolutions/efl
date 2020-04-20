f4ad86665d4bdbbbfdb8514c629e8de0e2fd4532
João Antônio Cardoso <joao.maker@gmail.com>
2020-04-20 18:46:08 -0300

    Cleaning for PR, deleting linux stuffs used for development

--- src/lib/evil/unposix/stdio.c
+++ src/lib/evil/unposix/stdio.c
@@ -1,35 +1,10 @@
-#ifdef LINUX
+#include "stdio.h"
+#include <stdio.h>
 
- #define _GNU_SOURCE                // needed for vsnprintf
- #define vasprintf _gnu_vasprintf   // revoking vasprintf
- #include <stdio.h>
- #undef vasprintf
-
-int
-vscprintf(const char *format, va_list ap)
-{
-   va_list ap_copy;
-   va_copy(ap_copy, ap);
-   int retval = vsnprintf(NULL, 0, format, ap_copy);
-   va_end(ap_copy);
-   return retval;
-}
-
-#else // WINDOWS
-
- #include "stdio.h"
- #include <stdio.h>
-  #define vscprintf _vscprintf
-  #define vsnprintf _vsnprintf
- #endif
+#define vscprintf _vscprintf
+#define vsnprintf _vsnprintf
 
 #include <stdlib.h>
-
-// REFS:
-// https://linux.die.net/man/3/vasprintf
-// https://stackoverflow.com/questions/40159892/using-asprintf-on-windows
-// https://rextester.com/BTNY65545
-// http://insanecoding.blogspot.com/2014/06/memory-management-in-c-and-auto.html
 
 int
 vasprintf(char **strp, const char *fmt, va_list ap)
--- src/tests/evil/unposix/vasprintf_test.c
+++ src/tests/evil/unposix/vasprintf_test.c
@@ -1,8 +1,3 @@
-#ifdef GNU
- #define _GNU_SOURCE
- #include <stdio.h>
-#endif
-
 #include "stdio.h"
 #include <stdarg.h>
 #include <stdlib.h>
@@ -14,55 +9,55 @@ int simple_test(void);
 
 int vasprintf_wrap(char **ret, const char *fmt, int nargs, ...)
 {
-  va_list ap;
-  va_start(ap, nargs);
+   va_list ap;
+   va_start(ap, nargs);
 
-  int len = vasprintf(ret, fmt, ap);
+   int len = vasprintf(ret, fmt, ap);
 
-  va_end(ap);
-  return len;
+   va_end(ap);
+   return len;
 }
 
 int
 simple_test(void)
 {
-  char *str;
-  int len;
+   char *str;
+   int len;
 
-  const char *expected_str = 
-    "\tString: Hello World!\n"
-    "\tInteger: 46234\n"
-    "\tDouble: 9.629400E-03\n";
+   const char *expected_str = 
+     "\tString: Hello World!\n"
+     "\tInteger: 46234\n"
+     "\tDouble: 9.629400E-03\n";
 
-  const char *fmt = 
-    "\tString: %s\n"
-    "\tInteger: %d\n"
-    "\tDouble: %E\n";
+   const char *fmt = 
+     "\tString: %s\n"
+     "\tInteger: %d\n"
+     "\tDouble: %E\n";
 
-  len = vasprintf_wrap(&str, fmt, 3, "Hello World!", 46234, 0.0096294);
+   len = vasprintf_wrap(&str, fmt, 3, "Hello World!", 46234, 0.0096294);
 
-  printf("## Expecting:\n```%s```\n", expected_str);
-  printf("## Returning:\n```%s```\n", str);
+   printf("## Expecting:\n```%s```\n", expected_str);
+   printf("## Returning:\n```%s```\n", str);
 
-  printf("\tResult:"); 
-  int result = strcmp(str, expected_str);
-  if(!result) printf("........ok!\n");
-  else        printf("........nok!\n"); 
+   printf("\tResult:"); 
+   int result = strcmp(str, expected_str);
+   if(!result) printf("........ok!\n");
+   else        printf("........nok!\n"); 
 
-  free(str);
-  return result;
+   free(str);
+   return result;
 }
 
 int
 main(void)
 {
 #ifdef GNU
-  printf("Test using vasprintf from GNU:\n");
+   printf("Test using vasprintf from GNU:\n");
 #else
-  printf("Test using custom vasprintf:\n");
+   printf("Test using custom vasprintf:\n");
 #endif
 
-  simple_test();
+   simple_test();
 
-  return 0;
+   return 0;
 }
