4206d8de4a7c7d28155b4a606cf522bfcaa0faa9
carlos <caiosignor@gmail.com>
2020-04-24 12:38:31 -0300

    code styles and fix meson.build

--- src/lib/evil/unposix/strings.c
+++ src/lib/evil/unposix/strings.c
@@ -1,11 +1,10 @@
 #include "strings.h"
 
-inline int ffs(int i)
+inline int
+ffs(int i)
 {
-	for (int j = 0; j < sizeof(i)*8; j++)
-	{
-		if (i & (1 << j))
-			return j;
-	}
-	return 0;
+   for (int j = 0; j < sizeof(i)*8; j++)	   
+	   if (i & (1 << j)) return j;
+     
+   return 0;
 }
