59a59a02162208f01a4267e64efad8b73b455e4d
Felipe Magno de Almeida <felipe@expertise.dev>
2020-04-30 13:25:55 -0300

    windows: Add test.bat and testing step for github actions

---
+++ test.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+set __OLD_vcvars64=%vcvars64%
+set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+set CLICOLOR_FORCE=1
+
+set NINJAFLAGS=%*
+
+set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+set CLICOLOR_FORCE=1
+
+if not defined DevEnvDir (
+    call %vcvars64%
+)
+
+ninja %NINJAFLAGS% -C build test
+
+set vcvars64=%__OLD_vcvars64%
+set __OLD_vcvars64=
+exit /B %ERRORLEVEL%
