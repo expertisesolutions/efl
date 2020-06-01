74527af7e0d5599f201e9a19aae990c181356ffc
Caue Baasch de Souza <cauebs@pm.me>
2020-06-01 10:48:42 -0300

    Swap raw comparisons for eina_thread_equals in eo

--- src/lib/eo/eo.c
+++ src/lib/eo/eo.c
@@ -1458,10 +1458,10 @@ eo_class_free(_Efl_Class *klass)
    void *data;
    Eina_Thread self = eina_thread_self();
 
-   if ((self != _efl_object_main_thread) &&
-       (self != klass->construction_thread))
-     CRI("Calling class deconstructor from thread that did not call constructor and is not main thread!\n"
-         "This will probably crash!");
+   if (eina_thread_equal(self, _efl_object_main_thread) &&
+       !eina_thread_equal(self, klass->construction_thread))
+      CRI("Calling class deconstructor from thread that did not call constructor and is not main thread!\n"
+          "This will probably crash!");
 
    if (klass->constructed)
      {
