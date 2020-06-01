86b3de3d0dcb22942d71c9c95e91e1e72969fe91
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2020-06-01 10:13:11 -0300

    add a .c stub file

---
+++ src/lib/ecore_win32/stub_ecore_funcs.c
@@ -0,0 +1,22 @@
+#include "stub_ecore_funcs.h"
+
+void* ecore_event_add(void * arg, ...)
+{
+   return 0;
+}
+
+void* ecore_event_init (void * arg, ...)
+{
+   return 0;
+}
+
+void* ecore_event_type_new (void * arg, ...)
+{
+   return 0;
+}
+
+void* ecore_event_shutdown (void * arg, ...)
+{
+   return 0;
+}
+
--- src/lib/ecore_win32/stub_ecore_funcs.h
+++ src/lib/ecore_win32/stub_ecore_funcs.h
@@ -4,9 +4,13 @@
 // We shoud try to separete ecore and ecore_input
 
 /*          ECORE FUNCS         */
-void * ecore_event_add(void * arg, ...) {
-  return 0;
-}
+void* ecore_event_add (void * arg, ...);
+
+void* ecore_event_init (void * arg, ...);
+
+void* ecore_event_type_new (void * arg, ...);
+
+void* ecore_event_shutdown (void * arg, ...);
 
 /*          ECORE_INPUT FUNCS         */
 #endif
