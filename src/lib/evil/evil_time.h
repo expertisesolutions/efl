984c3539dd537fc4a945e50514b5f248e4f9ce3e
João Antônio Cardoso <joao.antonio@expertisesolutions.com.br>
2020-06-05 00:37:24 -0300

    By-passing gettimeofday undefined errors

--- src/lib/evil/evil_time.h
+++ src/lib/evil/evil_time.h
@@ -5,6 +5,13 @@
 
 #include <time.h>
 
+#ifdef _WIN32
+# ifndef WIN32_LEAN_AND_MEAN
+#  define WIN32_LEAN_AND_MEAN
+# endif
+#include "winsock2.h" // strcut timeval
+#undef WIN32_LEAN_AND_MEAN
+#endif
 
 /**
  * @file evil_time.h
@@ -16,6 +23,28 @@
  *
  * @{
  */
+
+
+static int
+gettimeofday(struct timeval * tp, struct timezone * tzp)
+{
+   // This number is the number of 100 nanosecond intervals since 00:00:00
+   // 01/01/1601 (UTC) (Windows way) until 00:00:00 01/01/1970 (POSIX way)
+   static const uint64_t EPOCH = ((uint64_t) 116444736000000000ULL);
+
+   SYSTEMTIME system_time;
+   FILETIME file_time;
+   uint64_t time;
+
+   GetSystemTime(&system_time);
+   SystemTimeToFileTime( &system_time, &file_time );
+   time =  ((uint64_t)file_time.dwLowDateTime )      ;
+   time += ((uint64_t)file_time.dwHighDateTime) << 32;
+
+   tp->tv_sec  = (long) ((time - EPOCH) / 10000000L);
+   tp->tv_usec = (long) (system_time.wMilliseconds * 1000);
+   return 0;
+}
 
 
 /**
