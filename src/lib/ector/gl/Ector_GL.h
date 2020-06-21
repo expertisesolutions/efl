96b5469d97a19b9c1067c8fdb04835060c9a2b2f
Felipe Magno de Almeida <felipe@expertisesolutions.com.br>
2020-06-21 16:57:05 -0300

    ector: Change symbols for ector

--- src/lib/ector/gl/Ector_GL.h
+++ src/lib/ector/gl/Ector_GL.h
@@ -10,17 +10,17 @@
 #ifdef _WIN32
 # ifdef EFL_BUILD
 #  ifdef DLL_EXPORT
-#   define EAPI __declspec(dllexport)
+#   define ECTOR_API __declspec(dllexport)
 #  else
 #   define EAPI
 #  endif
 # else
-#  define EAPI __declspec(dllimport)
+#  define ECTOR_API __declspec(dllimport)
 # endif
 #else
 # ifdef __GNUC__
 #  if __GNUC__ >= 4
-#   define EAPI __attribute__ ((visibility("default")))
+#   define ECTOR_API __attribute__ ((visibility("default")))
 #  else
 #   define EAPI
 #  endif
