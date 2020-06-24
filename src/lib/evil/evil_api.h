0cffb1956b7be8445c0505544a4ec9776bb40cee
João Antônio Cardoso <joao.antonio@expertisesolutions.com.br>
2020-06-24 09:26:08 -0300

    Modified to use EVIL_BUILD

--- src/lib/evil/evil_api.h
+++ src/lib/evil/evil_api.h
@@ -2,25 +2,43 @@
 #define EVIL_API_H
 
 #ifdef EVIL_API
-#undef EVIL_API
+#error EVIL_API should not be already defined
 #endif
 
 #ifdef _WIN32
-# ifdef EFL_BUILD
-#  ifdef DLL_EXPORT
+# ifndef EVIL_STATIC
+#  ifdef EVIL_BUILD
 #   define EVIL_API __declspec(dllexport)
 #  else
-#   define EVIL_API
+#   define EVIL_API __declspec(dllimport)
 #  endif
-# else
-#  define EVIL_API __declspec(dllimport)
-# endif
-#else
-# if __GNUC__ >= 4
-#  define EVIL_API __attribute__ ((visibility("default")))
 # else
 #  define EVIL_API
 # endif
+# define EVIL_API_WEAK
+#else
+# ifdef __GNUC__
+#  if __GNUC__ >= 4
+#   define EVIL_API __attribute__ ((visibility("default")))
+#   define EVIL_API_WEAK __attribute__ ((weak))
+#  else
+#   define EVIL_API
+#   define EVIL_API_WEAK
+#  endif
+# else
+/**
+ * @def EVIL_API
+ * @brief Used to export functions (by changing visibility).
+ */
+#  define EVIL_API
+/**
+ * @def EINA_API_WEAK
+ * @brief Weak symbol, primarily useful in defining library functions which
+ * can be overridden in user code.
+ * Note: Not supported on all platforms.
+ */
+#  define EINA_API_WEAK
+# endif
 #endif
 
-#endif
+#endif // EVIL_API_H
