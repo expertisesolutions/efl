4e2207132cb91322ce0d5a685e46dafad03f731e
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2020-06-24 12:16:40 -0300

    Include edje_api instead of defining EDJE_API at Edje_Edit

--- src/lib/edje/Edje_Edit.h
+++ src/lib/edje/Edje_Edit.h
@@ -5,33 +5,8 @@
 #error "Do not use the Edje_Edit API unless you know what you are doing. It's meant only for writing editors and nothing else."
 #endif
 
+#include "edje_api.h"
 #include <Edje.h>
-
-#ifdef EAPI
-# undef EAPI
-#endif
-
-#ifdef _WIN32
-# ifdef EFL_BUILD
-#  ifdef DLL_EXPORT
-#   define EDJE_API __declspec(dllexport)
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EDJE_API __declspec(dllimport)
-# endif
-#else
-# ifdef __GNUC__
-#  if __GNUC__ >= 4
-#   define EDJE_API __attribute__ ((visibility("default")))
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EAPI
-# endif
-#endif
 
 /**
  * Compression type for the image.
