f201f746a1bb7f502bfd77bc608f97fa06e3256f
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2020-06-24 12:13:17 -0300

    Include edje_api instead of defining EDJE_API at edje_private.h

--- src/lib/edje/edje_private.h
+++ src/lib/edje/edje_private.h
@@ -64,34 +64,9 @@
 #endif
 
 #include "Edje.h"
+#include "edje_api.h"
 #define EFL_INTERNAL_UNSTABLE
 #include <Evas_Internal.h>
-
-#ifdef EAPI
-# undef EAPI
-#endif
-
-#ifdef _WIN32
-# ifdef EFL_BUILD
-#  ifdef DLL_EXPORT
-#   define EDJE_API __declspec(dllexport)
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EDJE_API __declspec(dllimport)
-# endif
-#else
-# ifdef __GNUC__
-#  if __GNUC__ >= 4
-#   define EDJE_API __attribute__ ((visibility("default")))
-#  else
-#   define EAPI
-#  endif
-# else
-#  define EAPI
-# endif
-#endif
 
 // This object is internal, only the interface needs to be exposed.
 #include "edje_global.eo.h"
