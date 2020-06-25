1af6553da2ac207a49967b57779352f153319f95
João Antônio Cardoso <joao.maker@gmail.com>
2020-06-25 08:39:41 -0300

    unistd imports fix (#168)
    * Fixing unistd includes
    
    * Adding a missing getopt

--- src/bin/eet/eet_main.c
+++ src/bin/eet/eet_main.c
@@ -5,7 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
+#ifndef _WIN32
+# include <unistd.h>
+#else
+# include <evil_private.h>
+#endif
 
 #include <Eet.h>
 
