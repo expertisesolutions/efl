910d678361f848ff6e8140be9ece16392a4751b3
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2020-06-30 11:36:06 -0300

    Define `ECORE_AUDIO_API_WEAK` as `ECORE_AUDIO_API` + weak definition
    So now is used as `ECORE_AUDIO_API_WEAK` instead of `ECORE_AUDIO_API ECORE_AUDIO_API_WEAK`

--- src/lib/ecore_audio/ecore_audio_api.h
+++ src/lib/ecore_audio/ecore_audio_api.h
@@ -15,12 +15,12 @@
 # else
 #  define ECORE_AUDIO_API
 # endif
-# define ECORE_AUDIO_API_WEAK
+# define ECORE_AUDIO_API_WEAK ECORE_AUDIO_API
 #else
 # ifdef __GNUC__
 #  if __GNUC__ >= 4
 #   define ECORE_AUDIO_API __attribute__ ((visibility("default")))
-#   define ECORE_AUDIO_API_WEAK __attribute__ ((weak))
+#   define ECORE_AUDIO_API_WEAK ECORE_AUDIO_API __attribute__ ((weak))
 #  else
 #   define ECORE_AUDIO_API
 #   define ECORE_AUDIO_API_WEAK
