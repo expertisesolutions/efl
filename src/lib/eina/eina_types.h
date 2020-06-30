34bb92bd3d841be7a31ac4ba254eecaa2824fd95
Lucas Cavalcante de Sousa <lucks.sousa@gmail.com>
2020-06-30 11:40:04 -0300

    Define `EINA_API_WEAK` as `EINA_API` + weak definition
    So now is used as `EINA_API_WEAK` instead of `EINA_API EINA_API_WEAK`

--- src/lib/eina/eina_types.h
+++ src/lib/eina/eina_types.h
@@ -47,12 +47,12 @@
 # else
 #  define EINA_API
 # endif
-# define EINA_API_WEAK
+# define EINA_API_WEAK EINA_API
 #else
 # ifdef __GNUC__
 #  if __GNUC__ >= 4
 #   define EINA_API __attribute__ ((visibility("default")))
-#   define EINA_API_WEAK __attribute__ ((weak))
+#   define EINA_API_WEAK EINA_API __attribute__ ((weak))
 #  else
 #   define EINA_API
 #   define EINA_API_WEAK
