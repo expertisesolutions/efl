d9eb3da4ab6e4a614194f0c8b9d40aad28ea7b20
Lucas <Coquinho@users.noreply.github.com>
2020-06-30 17:14:23 -0300

    [edje] Change EAPI symbols for EDJE_API (#178)
    * Change symbols for edje
    * Define -DEDJE_BUILD at it meson.build
    * Include edje_api instead of defining EDJE_API at edje_private.h
    * Include edje_api instead of defining EDJE_API at Edje_Edit
    
    Co-authored-by: Felipe Magno de Almeida <felipe@expertisesolutions.com.br>

--- src/lib/edje/edje_edit_eo.h
+++ src/lib/edje/edje_edit_eo.h
@@ -23,6 +23,6 @@ typedef Eo Edje_Edit;
  */
 #define EDJE_EDIT_CLASS edje_edit_class_get()
 
-EWAPI const Efl_Class *edje_edit_class_get(void);
+EDJE_API EDJE_WEAK_API const Efl_Class *edje_edit_class_get(void);
 
 #endif
