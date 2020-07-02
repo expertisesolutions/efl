aed6b5ae9b4deef812a4295953b517ca52ee8fff
Lucas <Coquinho@users.noreply.github.com>
2020-07-02 13:21:34 -0300

    [Ecore_con] Correctly define API (#200)
    [Ecore_con] Correctly define API
    
    extern EAPI -> EAPI extern
    extern EWAPI -> EWAPI extern
    EOAPI -> ECORE_CON_API ECORE_CON_API_WEAK
    EWAPI -> ECORE_CON_API ECORE_CON_API_WEAK

--- src/lib/ecore_con/ecore_con_eet_server_obj_eo.h
+++ src/lib/ecore_con/ecore_con_eet_server_obj_eo.h
@@ -19,6 +19,6 @@ typedef Eo Ecore_Con_Eet_Server_Obj;
  */
 #define ECORE_CON_EET_SERVER_OBJ_CLASS ecore_con_eet_server_obj_class_get()
 
-ECORE_CON_API ECORE_CON_WEAK_API const Efl_Class *ecore_con_eet_server_obj_class_get(void);
+ECORE_CON_API ECORE_CON_API_WEAK const Efl_Class *ecore_con_eet_server_obj_class_get(void);
 
 #endif
