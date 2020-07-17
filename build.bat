8e2891e83f1d8d9870a9ba3dbade9313d0e4ba8c
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2020-07-17 20:56:18 -0300

    build: Check VS edition instead of assuming Professional (#315)
    Resolves #308.

--- build.bat
+++ build.bat
@@ -1,19 +1,11 @@
 @echo off
 
 set __OLD_vcvars64=%vcvars64%
-set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
 set CLICOLOR_FORCE=1
 
 set NINJAFLAGS=%*
 
-set vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
-set CLICOLOR_FORCE=1
-
-if not defined DevEnvDir (
-    echo DevEnvDir not defined. Running vcvars64...
-    call %vcvars64%
-    @set DevEnvDir=%cd%
-)
+call ensure-vcvars64
 
 ninja %NINJAFLAGS% -C build
 
