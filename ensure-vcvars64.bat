01505e6f9d8a6f3acb9d88665901967a1d0a0c07
João Paulo Taylor Ienczak Zanette <jpaulotiz@gmail.com>
2020-07-21 17:35:10 -0300

    build: Fix usage of EnableDelayedExpansion (#328)
    Use setlocal only in very restricted scopes (instead of globally). This way, calls to vcvars64 will make effect for callee scripts.

--- ensure-vcvars64.bat
+++ ensure-vcvars64.bat
@@ -1,32 +1,36 @@
 @echo off
-:: Needed because batch reads lines long before executing them, which means it
-:: will expand variables before it has even been set (yes), so sometimes they
-:: will come with trash-or-none value. This enables the usage of `!variable!`,
-:: which (differently from `%variable%`) expands the variable value when
-:: executing.
-setlocal EnableDelayedExpansion
+:: About `setlocal EnableDelayedExpansion`:
+::   Needed because batch reads lines long before executing them, which means
+::   it will expand variables before it has even been set (yes), so sometimes
+::   they will come with trash-or-none value. This enables the usage of
+::   `!variable!`, which (differently from `%variable%`) expands the variable
+::   value when executing.
 
 call :main || (echo Failed to ensure vcvars64 is found)
 exit /B %errorlevel%
 
 :check_path
-    where vcvars64 2>nul
-    if %errorlevel% == 0 (
+    set vcvars_in_path=0
+    (
+        where vcvars64 2>nul
+        call set vcvars_in_path=%%errorlevel%%
+    )
+    if "%vcvars_in_path%" == "0" (
         echo [GOOD] vcvars64 found in PATH.
-        :: Assign command output to variable (...wtf batch)
-        for /f %%i in ('where vcvars64') do set vcvars64=%%i
+        vcvars64
+        exit /B 0
     ) else (
         echo [INFO] vcvars64 not found in PATH.
-	)
+    )
 exit /B 0
 
 :try_version
     set edition=%~1
     set _vcvars64="C:\Program Files (x86)\Microsoft Visual Studio\2019\%edition%\VC\Auxiliary\Build\vcvars64.bat"
-    if not exist !_vcvars64! (
+    if not exist %_vcvars64% (
         exit /B 1
 	)
-    set vcvars64=!_vcvars64!
+    call set vcvars64=%%_vcvars64%%
 exit /B 0
 
 :find_vs_vcvars64
@@ -35,10 +39,10 @@ exit /B 0
     for %%v in (Enterprise Professional Community) do (
         echo Trying edition: %%v
         call :try_version %%v
-        if "!vcvars64!" == "" (
+        if not defined vcvars64 (
             echo [INFO] No vcvars64 for "%%v" edition
         ) else (
-            echo [GOOD] Found vcvars in !vcvars64!
+            call echo [GOOD] Found vcvars in %%vcvars64%%
             exit /B 0
         )
 	)
@@ -49,11 +53,11 @@ exit /B 0
         echo DevEnvDir not set, looking for vcvars64...
         call :check_path
 
-        if "!vcvars64!" == "" (
+        if not defined vcvars64 (
             call :find_vs_vcvars64
         )
-        echo vcvars64: !vcvars64!
-        !vcvars64!
+        call echo vcvars64: %%vcvars64%%
+        call %%vcvars64%%
     ) else (
         echo DevEnvDir already set. Nothing to be done here.
 	)
