edb1943a8785a014e46b638100f6d8e59b0e9bd8
João Antônio Cardoso <joao.maker@gmail.com>
2020-07-28 17:26:46 -0300

    Reimplement basename for windows considering multi-threads (#323)
    - Using a local storage to enable the usage with multiples threads

--- src/lib/evil/evil_basename.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EVIL_BASENAME_H__
-#define __EVIL_BASENAME_H__
-
-#include "evil_private.h"
-
-EVIL_API char* basename (char* path);
-
-#endif
-
--- src/tests/evil/evil_test_libgen.c
+++ src/tests/evil/evil_test_libgen.c
@@ -9,6 +9,86 @@
 
 #include "evil_suite.h"
 
+EFL_START_TEST(evil_libgen_basename_simple)
+{
+   char path[] = "/bin/foo/bar.h";
+   char expected[] = "bar.h";
+   char *res = evil_basename(path);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
+EFL_START_TEST(evil_libgen_basename_short)
+{
+   char path[] = "a";
+   char expected[] = "a";
+   char *res = evil_basename(path);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
+EFL_START_TEST(evil_libgen_basename_root)
+{
+   char path[] = "/";
+   char expected[] = "/";
+   char *res = evil_basename(path);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
+EFL_START_TEST(evil_libgen_basename_trailing)
+{
+   char path[] = "/bin/foo/";
+   char expected[] = "foo";
+
+   char *res = evil_basename(path);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
+EFL_START_TEST(evil_libgen_basename_empty)
+{
+   char path[] = "";
+   char expected[] = ".";
+   char *res = evil_basename(path);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
+EFL_START_TEST(evil_libgen_basename_null)
+{
+   char expected[] = ".";
+   char *res = evil_basename(NULL);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
+EFL_START_TEST(evil_libgen_basename_filename)
+{
+   char path[] = "bar.h";
+   char expected[] = "bar.h";
+   char *res = evil_basename(path);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
+EFL_START_TEST(evil_libgen_basename_windows)
+{
+   char path[] = "C:\\foo\\bar.h";
+   char expected[] = "bar.h";
+   char *res = evil_basename(path);
+
+   fail_if(strcmp(res, expected) != 0);
+}
+EFL_END_TEST
+
 EFL_START_TEST(evil_libgen_dirname_simple)
 {
    char path[] = "/bin/foo/bar.h";
@@ -91,6 +171,15 @@ EFL_END_TEST
 
 void evil_test_libgen(TCase *tc)
 {
+   tcase_add_test(tc, evil_libgen_basename_simple);
+   tcase_add_test(tc, evil_libgen_basename_short);
+   tcase_add_test(tc, evil_libgen_basename_root);
+   tcase_add_test(tc, evil_libgen_basename_trailing);
+   tcase_add_test(tc, evil_libgen_basename_empty);
+   tcase_add_test(tc, evil_libgen_basename_null);
+   tcase_add_test(tc, evil_libgen_basename_filename);
+   tcase_add_test(tc, evil_libgen_basename_windows);
+
    tcase_add_test(tc, evil_libgen_dirname_simple);
    tcase_add_test(tc, evil_libgen_dirname_short);
    tcase_add_test(tc, evil_libgen_dirname_root);
