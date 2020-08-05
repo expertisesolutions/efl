9d2481167cafbcff1a6797cdf72c5e66223ec167
João Antônio Cardoso <joao.maker@gmail.com>
2020-08-05 11:30:00 -0300

    Fix double free and memory leak

--- src/bin/elementary/embed_config.c
+++ src/bin/elementary/embed_config.c
@@ -1,4 +1,3 @@
-
 #include <assert.h>
 #include <errno.h>
 #include <getopt.h>
@@ -7,27 +6,27 @@
 #include <string.h>
 
 #ifdef HAVE_CONFIG1
-#include "elementary_config.h"
+# include "elementary_config.h"
 #endif
 
 static void
-_print_usage(const char *progn, FILE *outf)
+_print_usage(const char *progn)
 {
-   fprintf(outf, "Usage: %s [options] [input]\n", progn);
-   fprintf(outf, "Options:\n"
+   fprintf(stdout, "Usage: %s [options] [input]\n", progn);
+   fprintf(stdout, "Options:\n"
                  "  -o output     specify the filename and path for output\n"
                  "  -v            show version number\n"
                  "\n");
 }
 
-char* _read_input_file (FILE* f, const char* fname)
+char *
+_read_input_file(FILE* f, const char* fname)
 {
    fseek(f, 0, SEEK_END);
    long fs = ftell(f);
    if (fs < 0)
      {
         fprintf(stderr, "eolian: could not get length of '%s'\n", fname);
-        fclose(f);
         return NULL;
      }
    fseek(f, 0, SEEK_SET);
@@ -36,7 +35,7 @@ char* _read_input_file (FILE* f, const char* fname)
    if (!cont)
      {
         fprintf(stderr, "eolian: could not allocate memory for '%s'\n", fname);
-        fclose(f);
+        free(cont);
         return NULL;
      }
 
@@ -46,16 +45,15 @@ char* _read_input_file (FILE* f, const char* fname)
         fprintf(stderr, "eolian: could not read %ld bytes from '%s' (got %ld)\n",
                 fs, fname, as);
         free(cont);
-        fclose(f);
         return NULL;
      }
 
    cont[fs] = '\0';
-   fclose(f);
    return cont;
 }
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
    char *output = NULL, *input = NULL;
    FILE *fin = NULL, *fout = NULL;
@@ -70,40 +68,40 @@ int main(int argc, char** argv)
           output = strdup(optarg);
           break;
         case 'h':
-          _print_usage(argv[0], stdout);
+          _print_usage(argv[0]);
           return 0;
        }
 
    if (!argv[optind])
      {
         fprintf(stderr, "embed_config: no input file\n");
-        goto no_input;
+        goto clean_return;
      }
    else
      input = strdup(argv[optind]);
 
-   
-   fin = fopen (input, "rb");
-   fout = fopen (output, "wb");
+   fin = fopen(input, "rb");
    if (!fin)
      {
         fprintf(stderr, "embed_config: could not open '%s' (%s)\n",
                 input, strerror(errno));
-        goto not_open_in;
+        goto clean_return;
      }
+
+   fout = fopen(output, "wb");
    if (!fout)
      {
         fprintf(stderr, "embed_config: could not open '%s' (%s)\n",
                 output, strerror(errno));
-        goto not_open_out;
+        goto clean_return;
      }
 
-   char* input_buffer = _read_input_file (fin, input);
+   char *input_buffer = _read_input_file(fin, input);
    if (input_buffer)
      {
         const char file_prefix[] = "static const char *embedded_config = \"\"\n";
         const char file_suffix[] = ";\n";
-        fwrite (&file_prefix[0], sizeof(file_prefix) -1, 1, fout);
+        fwrite(&file_prefix[0], sizeof(file_prefix) -1, 1, fout);
         unsigned int i = 0, eol = 0, eof = strlen(input_buffer);
         
         while (i != eof)
@@ -116,35 +114,34 @@ int main(int argc, char** argv)
 
             while (i != eol)
               {
-                if (input_buffer[i] == '"')
-                {
-                  fputc('\\', fout);
-                }
-                fputc(input_buffer[i], fout);
-                ++i;
+                 if (input_buffer[i] == '"')
+                   fputc('\\', fout);
+                 fputc(input_buffer[i], fout);
+                 ++i;
               }
             fputs("\\n\"", fout);
             if (i != eof)
-            {
-              assert (i == eol);
-              assert (input_buffer[i] == '\n' || input_buffer[i] == '\r');
-              while (input_buffer[i] == '\r' || input_buffer[i] == '\n')
-                fputc(input_buffer[i++], fout);
-            }
+              {
+                 assert(i == eol);
+                 assert(input_buffer[i] == '\n' || input_buffer[i] == '\r');
+                 while (input_buffer[i] == '\r' || input_buffer[i] == '\n')
+                   fputc(input_buffer[i++], fout);
+              }
           }
 
-        fwrite (&file_suffix[0], sizeof(file_suffix) -1, 1, fout);
+        fwrite(&file_suffix[0], sizeof(file_suffix) -1, 1, fout);
 
         err = 0;
      }
    else
      fprintf(stderr, "embed_config: could not read from '%s'\n", input);
-     
-not_open_out:
-   fclose(fout);
-not_open_in:
-   fclose(fin);
-no_input:
-  free(output);
-  return err;
+
+   free(input_buffer);
+
+clean_return:
+   if (fin != NULL) fclose(fin);
+   if (fout != NULL) fclose(fout);
+   free(input);
+   free(output);
+   return err;
 }
