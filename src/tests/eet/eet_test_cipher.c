4275bb8de41ba160d143d0c3195df046d7dd0ef4
João Antônio Cardoso <joao.maker@gmail.com>
2020-08-21 08:19:05 -0300

    Eet: Resolve eet_test_cipher test case (#392)
    Like others `eet` test cases, this test case had 2 problems one while creating a temporary file and other while closing it:
    
    About creating the file: `/tmp` doesn't exist in windows and `mkstemp` expects that all path given already exists (besides the file it will create). Here this is solved by using `eina_file_mkstemp` that creates a temporary file at the temporary directory obtained with `eina_envitonment_tmp` which is an architecture-independent function.
    
    About closing it: `eet_close` is a postponed close, it doesn't immediately close files opened as `write and read` nor `write` unless it has non saved changes nor immediately closes files opened as `read` files. So when arriving at `unlink`
    the file may not be closed yet. 
    On linux it isn't a problem, as when `unlink`ing an open file it returns `0` and waits until all references to the file to be closed for its removal. But on windows while `unlink`ing an open file `-1` is returned, setting `errno` to `13`(`strerror(13)` = `Permission denied`).
    
    This is solved using `eet_clearcache` which forces the closure of every file (in case the file has been altered it flushes it before closing).
    Note that this solves the problem for *this* test, but it isn't the best solution, an user still needs to know where his program will run to know if they need to force file closure before  `unlink`.
    
    Co-authored-by: Lucas <Coquinho@users.noreply.github.com>

--- src/tests/eet/eet_test_cipher.c
+++ src/tests/eet/eet_test_cipher.c
@@ -24,15 +24,17 @@ EFL_START_TEST(eet_test_cipher_decipher_simple)
    const char *key_bad = "This is another crypto key";
    Eet_File *ef;
    char *test;
-   char *file = strdup("/tmp/eet_suite_testXXXXXX");
    int size;
+   Eina_Tmpstr* tmpfile = NULL;
    int tmpfd;
 
-   fail_if(-1 == (tmpfd = mkstemp(file)));
+   /* tmpfile will be created in temporary directory (with eina_environment_tmp) */
+   tmpfd = eina_file_mkstemp("eet_suite_testXXXXXX", &tmpfile);
+   fail_if(-1 == tmpfd);
    fail_if(!!close(tmpfd));
 
    /* Crypt an eet file. */
-   ef = eet_open(file, EET_FILE_MODE_WRITE);
+   ef = eet_open(tmpfile, EET_FILE_MODE_WRITE);
    fail_if(!ef);
 
    fail_if(!eet_write_cipher(ef, "keys/tests", buffer, strlen(buffer) + 1, 0,
@@ -41,7 +43,7 @@ EFL_START_TEST(eet_test_cipher_decipher_simple)
    eet_close(ef);
 
    /* Decrypt an eet file. */
-   ef = eet_open(file, EET_FILE_MODE_READ);
+   ef = eet_open(tmpfile, EET_FILE_MODE_READ);
    fail_if(!ef);
 
    test = eet_read_cipher(ef, "keys/tests", &size, key);
@@ -53,7 +55,7 @@ EFL_START_TEST(eet_test_cipher_decipher_simple)
    eet_close(ef);
 
    /* Decrypt an eet file. */
-   ef = eet_open(file, EET_FILE_MODE_READ);
+   ef = eet_open(tmpfile, EET_FILE_MODE_READ);
    fail_if(!ef);
 
    test = eet_read_cipher(ef, "keys/tests", &size, key_bad);
@@ -62,8 +64,15 @@ EFL_START_TEST(eet_test_cipher_decipher_simple)
      fail_if(memcmp(test, buffer, strlen(buffer) + 1) == 0);
 
    eet_close(ef);
+   /* As `eet_close` is a postponed close and windows' `unlink` doesn't execute
+    * successfully if there is any reference to the file, here `eet_clearcache`
+    * is used to assure that the file is really closed when the unlink happens.
+    */
+   eet_clearcache();
 
-   fail_if(unlink(file) != 0);
+   fail_if(unlink(tmpfile) != 0);
+   eina_tmpstr_del(tmpfile);
+
 }
 EFL_END_TEST
 
