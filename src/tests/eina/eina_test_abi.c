6b289ae6b8d1422a7cac8a82c51b869d5f9f8599
Felipe Magno de Almeida <felipe@expertise.dev>
2020-10-26 18:33:46 -0300

    Commenting out to remove a warning.
    warning: attribute declaration must precede definition

--- src/tests/eina/eina_test_abi.c
+++ src/tests/eina/eina_test_abi.c
@@ -43,8 +43,10 @@ typedef uint32_t Eina_Unicode;
 #include "eina_main.h"
 #include "eina_safety_checks.h"
 
-EAPI Eina_Unicode eina_unicode_utf8_get_next(const char *buf, int *iindex);
-EAPI unsigned int eina_mempool_alignof(unsigned int size);
+/*
+EINA_API Eina_Unicode eina_unicode_utf8_get_next(const char *buf, int *iindex);
+EINA_API unsigned int eina_mempool_alignof(unsigned int size);
+*/
 
 EFL_START_TEST(eina_unicode_utf8)
 {
