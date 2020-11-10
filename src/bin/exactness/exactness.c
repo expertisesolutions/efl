281b6423c552359613866033365d7b064a1884ac
João Antônio Cardoso <joao.maker@gmail.com>
2020-11-10 12:07:56 -0300

    exactness: Fix relative path for the commands

--- src/bin/exactness/exactness.c
+++ src/bin/exactness/exactness.c
@@ -352,6 +352,9 @@ _list_file_load(const char *filename)
         return NULL;
      }
 
+   char *_test_file_dir = realpath(filename, NULL);
+   _test_file_dir = dirname(_test_file_dir);
+
    while (fgets(buf, BUF_SIZE, file))
      {
         /* Skip comment/empty lines. */
@@ -367,7 +370,9 @@ _list_file_load(const char *filename)
         if (tmp)
           {
              *tmp = '\0';
-             cur->command = tmp + 1;
+	     char *command;
+	     asprintf(&command, "%s/%s", _test_file_dir, tmp + 1);
+             cur->command = command;
           }
         else
           {
@@ -386,6 +391,8 @@ _list_file_load(const char *filename)
               eina_inlist_append(EINA_INLIST_GET(ret), EINA_INLIST_GET(cur)),
               List_Entry);
      }
+
+   free(_test_file_dir);
 
    fclose(file);
    return ret;
